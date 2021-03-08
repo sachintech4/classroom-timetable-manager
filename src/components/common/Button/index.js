47c8d7b42485f56b5469ad78d9148af20c63aeb4
Sandeep-Tech <sandeeptech8@gmail.com>
2021-03-08 16:24:45 +0530

    added a common components folder

---
+++ src/components/common/Button/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./index.module.css";
+
+const Button = (props) => {
+  return (
+    <div className={styles.container}>
+      <button className={styles.button}>{props.buttonText}</button>
+    </div>
+  );
+};
+
+export default Button;
