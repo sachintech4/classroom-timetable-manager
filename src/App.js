613e39146be07810b7d73a6999dd86f8db0a0444
Sandeep-Tech <sandeeptech8@gmail.com>
2021-03-08 16:24:20 +0530

    added basic content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.module.css';
+import styles from "./App.module.css";
+import Button from "./components/common/Button";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <h1>Thakur college of science and commerce</h1>
+      <h2>Classroom management system</h2>
+      <h3>Select user type</h3>
+      <Button buttonText={"Admin"}/>
+      <Button buttonText={"Teacher"}/>
+      <Button buttonText={"Student"}/>
     </div>
   );
-}
+};
 
 export default App;
