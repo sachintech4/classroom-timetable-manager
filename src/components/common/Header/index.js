7ee098514f46eb7df5d032f00f088590e1a6d76f
Sandeep-Tech <sandeeptech8@gmail.com>
2021-03-08 17:27:12 +0530

    added assets folder, logo and did some changes in the page

---
+++ src/components/common/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./index.module.css";
+import logo from "../../../assets/logo/tcsc_logo.png";
+
+const Header = () => {
+  return (
+    <div className={styles.container}>
+      <img src={logo} alt="College logo" className={styles.logo} />
+
+      <h1>Thakur college of science and commerce</h1>
+    </div>
+  );
+};
+
+export default Header;
